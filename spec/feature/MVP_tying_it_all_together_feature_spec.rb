c490ce1db4612cfdc508242800ae07d6d9f13a87
Richard Lynch <rjlynch86@googlemail.com>
2015-05-11 16:39:04 +0100

    new test for you

--- spec/feature/MVP_tying_it_all_together_feature_spec.rb
+++ spec/feature/MVP_tying_it_all_together_feature_spec.rb
@@ -1,4 +1,8 @@
+require 'spec_helper'  
+
+feature "Feature Test" do
   scenario 'Seeing reviews for cinema', js: true do
+    %x(say 'Feature Test')
     visit '/postcode'
     fill_in('Postcode', with: 'EC1V 8AZ')
     click_link 'Barbican Cinemas'
@@ -6,3 +10,4 @@
     expect(page).to have_content 'IMDb: 7.2'
     expect(page).to have_content 'Rotten Tomatoes: 7.7'
   end
+end
