af1c56ec31595843ee2a8afc585356150109bbf3
Richard Lynch <rjlynch86@googlemail.com>
2015-05-11 13:25:49 +0100

    can select a movie from the drop down list

--- spec/feature/mvp_feature_spec.rb
+++ spec/feature/mvp_feature_spec.rb
@@ -7,4 +7,10 @@ feature 'MVP' do
     expect(page).to have_css('select')
   end
 
+  scenario 'can select a movie from the drop down list' do
+    visit '/'
+    result = find('select').find(:option, 'Home').text
+    expect(result).to eq'Home'
+  end
+
 end
