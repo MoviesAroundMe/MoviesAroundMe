49afe236dbd1eb49e9640f21ddbf6361c5fc2f8e
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-05-12 11:18:41 +0100

    static files served

--- app/movies_around_me.rb
+++ app/movies_around_me.rb
@@ -6,6 +6,8 @@ class MoviesAroundMe < Sinatra::Base
 
   register Sinatra::CrossOrigin
   enable :cross_origin
+  set :root, File.dirname(__FILE__)
+  set :public_folder, Proc.new {File.join(root, '../public')}
 
   options '/*' do
     response["Access-Control-Allow-Headers"] = "origin, x-requested-with, content-type"
